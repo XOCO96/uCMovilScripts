13da71abcb195d23e5f5fd8b3d9c44376f74781c
XOCO96 <lalo_1010_@hotmail.com>
2020-07-03 14:57:37 -0500

    Agregado código para un contador basado en la interrupción por puerto

--- Interrupciones/Puerto/Puerto.ino
+++ Interrupciones/Puerto/Puerto.ino
@@ -1,9 +1,29 @@
+/* Script de led intermitente y contador, haciendo uso de la función
+    de interrupción externa por puerto  */
+
+
+const int BOTON_INT = 0;
+int cuenta = 0;
+boolean estado = true;
+
+
 void setup()
 {
-    
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
+    attachInterrupt(BOTON_INT, swap, RISING);
 }
 
 void loop()
 {
+    digitalWrite(LED_BUILTIN,estado);
+    Serial.println(cuenta);
+    delay(1000);
+}
 
+
+void swap()
+{
+    cuenta++;
+    estado = !estado;
 }
